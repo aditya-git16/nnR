f406c4f4983fed9d9724a4c47bfb914c72a20446
Aditya <es1221211@iitd.ac.in>
2026-07-01 00:51:03 +0530

    add linear function and calculate error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use nn_r::model::prediction;
+use nn_r::util::linear;
 
 fn main() {
-    let prediction = prediction(2);
+    let x = 4;
+    let prediction = prediction(x);
     println!("{}" , prediction);
+    let actual_value = linear(x);
+    println!("{}" , actual_value);
+    let error = prediction - actual_value;
+    let error_absolute  = (prediction - actual_value).abs();
+    println!("error  = {} , error_absolute = {}" , error , error_absolute);
 }
