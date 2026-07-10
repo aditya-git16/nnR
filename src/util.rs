0a34e274c59e080400b5640118cc7b671449ee4f
Aditya <es1221211@iitd.ac.in>
2026-07-10 13:22:34 +0530

    define new mv linear function

--- src/util.rs
+++ src/util.rs
@@ -12,3 +12,9 @@ pub fn mv_linear(sample: Sample) -> f64 {
     let constant = 12.0;
     return 3.2 * sample[0] + 1.3 * sample[1] + constant;
 }
+
+// define new multi variable linear function for multiple output testing
+pub fn mv_linear_n(sample: Sample) -> f64 {
+    let constant = 13.2;
+    return 1.6 * sample[0] + 2.8 * sample[1] + constant;
+}
